c3e1c8b246329f7e906e6d2d32a0117cf6b6f759
Shreyansh Srivastava <shreyansh.official.6726@gmail.com>
2026-01-29 17:49:27 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const app = express();
 
 // 1. DATABASE CONNECTION (Updated for Deployment)
 // Uses process.env for production and local string for testing
-const MONGO_URI = process.env.MONGO_URI || 'mongodb+srv://shreyanshofficial6726_db_user:qVhqqTxxfadmddec@cluster0.ylrjh3i.mongodb.net/users';
+const MONGO_URI = process.env.MONGO_URI || 'mongodb+srv://shreyanshofficial6726_db_user:qVhqqTxxfadmddec@cluster0.ylrjh3i.mongodb.net/';
 
 mongoose.connect(MONGO_URI)
     .then(() => console.log("MongoDB Connected..."))
